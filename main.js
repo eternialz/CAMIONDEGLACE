65782ee94537bbe47071a2b1a62473d37b1310c4
Sébastien Erfani <sebastien.erfani@supinfo.com>
2019-01-25 21:15:21 +0100

    Add P5

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 1280, height: 720 })
+    // Create the browser window.
+    win = new BrowserWindow({ width: 1280, height: 720 })
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+    // and load the index.html of the app.
+    win.loadFile('index.html')
 }
 
 app.on('ready', createWindow)
