261182a9b67c7c8e1b58cbb2d31179bb67bead9d
Sébastien Erfani <sebastien.erfani@supinfo.com>
2019-01-25 21:24:15 +0100

    Add borderless windows

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({ width: 1280, height: 720 })
-
+    win = new BrowserWindow({ width: 1280, height: 720, frame: false });
+    win.setMenu(null);
     // and load the index.html of the app.
-    win.loadFile('index.html')
+    win.loadFile('index.html');
 }
 
 app.on('ready', createWindow)
