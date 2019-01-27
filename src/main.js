7cda55883c05e208074a01c3c5d44c855bf48c79
Sébastien Erfani <sebastien.erfani@gmail.com>
2019-01-27 10:48:34 +0100

    Add sounds and p5

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 const { app, BrowserWindow } = require('electron');
+var path = require('path');
 
-function createWindow () {
+function createWindow() {
     // Create the browser window.
-    let win = new BrowserWindow({ width: 1280, height: 757});
+    let win = new BrowserWindow({
+        width: 1280,
+        height: 757,
+        title: 'Camion De Glace - GGJ2019',
+        icon: path.join(__dirname, 'assets/icons/128x128.png'),
+    });
 
     win.setMenu(null);
 
--- src/stimulus.js
+++ src/stimulus.js
@@ -1,12 +1,16 @@
-import SceneController from "./controllers/scene_controller";
-import UserInterfaceController from "./controllers/user_interface_controller";
+import SceneController from './controllers/scene_controller';
+import UserInterfaceController from './controllers/user_interface_controller';
 
 const application = Stimulus.Application.start();
-        
-application.register("hello", class extends Stimulus.Controller {
-static get targets() {
-    return [ "name" ]
-}});
 
-application.register("scene", SceneController);
-application.register("user-interface", UserInterfaceController);
+application.register(
+    'hello',
+    class extends Stimulus.Controller {
+        static get targets() {
+            return ['name'];
+        }
+    }
+);
+
+application.register('scene', SceneController);
+application.register('user-interface', UserInterfaceController);
